a9cee189a495423c118ce1a4c5cf480b98b24a31
entrolEC <euichan1108@gmail.com>
2023-07-05 03:04:57 +0900

    Chore: 추상화 색상 스타일 추가 및 기본 스타일 제거

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,13 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      'bg-primary': '#F1F5F9',
+      'bg-secondary': '#D9D9D9',
+
+      'primary': '#5D5FEF',
+      'secondary': '#A5A6F6'
+    }
   },
   plugins: [],
 }
