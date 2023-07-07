47af0efb33dcdbdaa737ff19d8ebcded86481dd2
entrolEC <euichan1108@gmail.com>
2023-07-07 21:46:29 +0900

    Feat: 선택 아이템 테두리 스타일 추가

--- src/app/quiz/quiz-items.tsx
+++ src/app/quiz/quiz-items.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client"
+import React, {useCallback, useState} from "react";
 import { QuizItem} from "@/app/quiz/types";
 
 const optionSignature = [
@@ -8,21 +9,39 @@ const optionSignature = [
     'D'
 ]
 
-export const QuizItems = ({quizItems}: {quizItems: QuizItem[]}) => (
-    <div className="container flex flex-col">
-        {
-            quizItems.map((item, idx) => {
-                const itemString = `${optionSignature[idx]}. ${item.item_content}`
-                return (
-                    <QuizItem key={`quiz_item_${idx}`} itemString={itemString}/>
-                )
-            })
-        }
-    </div>
-)
+export const QuizItems = ({quizItems}: {quizItems: QuizItem[]}) => {
+    const [selecedItem, setSelectedItem] = useState<null | number>(null);
 
-const QuizItem = ({itemString}: { itemString: string}) => (
-    <div className="h-14 border-2 rounded-lg border-bg-primary shadow-lg shadow-bg-primary flex items-center justify-start px-4 my-1 text-sm">
+    const handleClicked = useCallback((selectedIndex: number) => {
+        setSelectedItem(selectedIndex);
+    }, [])
+
+    return (
+        <div className="container flex flex-col">
+            {
+                quizItems.map((item, idx) => {
+                    const itemString = `${optionSignature[idx]}. ${item.item_content}`
+                    return (
+                        <QuizItem
+                            key={`quiz_item_${idx}`}
+                            itemString={itemString}
+                            selected={idx === selecedItem}
+                            idx={idx}
+                            handleClicked={handleClicked}
+                        />
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+const QuizItem = ({itemString, selected, idx, handleClicked}: { itemString: string, selected: boolean, idx: number, handleClicked: (selectedIndex: number) => void}) => (
+    <div
+        className={`h-14 border-2 rounded-lg shadow-lg shadow-bg-primary flex items-center justify-start px-4 my-1 text-sm
+        ${selected ? 'border-primary' : 'border-bg-primary'}`}
+        onClick={() => handleClicked(idx)}
+    >
         {itemString}
     </div>
 )
