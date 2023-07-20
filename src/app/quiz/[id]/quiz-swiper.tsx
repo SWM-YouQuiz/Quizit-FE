3057844140088e3c8ad2b91f22663a5b6a7d2781
entrolEC <euichan1108@gmail.com>
2023-07-20 12:47:12 +0900

    Chore: 불필요한 코드 제거

--- src/app/quiz/[id]/quiz-swiper.tsx
+++ src/app/quiz/[id]/quiz-swiper.tsx
@@ -1,11 +1,8 @@
 "use client"
 
 import React, {ReactNode, Suspense} from 'react';
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
 
-// Import Swiper styles
 import 'swiper/css';
 
 const QuizSwiper = ({children}: {children: ReactNode}) => {
