7bda33d7fb5394e218fb12f3aaaba973771f051f
entrolEC <euichan1108@gmail.com>
2023-07-20 15:33:13 +0900

    Feat: 풀이 화면 네비게이션 구현

--- src/app/quiz/[id]/quiz-swiper.tsx
+++ src/app/quiz/[id]/quiz-swiper.tsx
@@ -7,10 +7,13 @@ import 'swiper/css';
 
 const QuizSwiper = ({children}: {children: ReactNode}) => {
     return (
-        <Swiper navigation={true} spaceBetween={4} initialSlide={0} className="container h-full w-full" onSlideChange={() => {}}>
+        <Swiper className="container h-full w-full" navigation={true} spaceBetween={4} initialSlide={0} onSlideChange={() => {}}>
             {React.Children.map(children, (child, index) => (
-                <SwiperSlide key={`quiz-${index}`} style={{display: "flex", flexDirection: "column"}}>
-                    {child}
+                <SwiperSlide key={`quiz-${index}`}>
+                    <Swiper className="h-full" navigation={true} spaceBetween={4} initialSlide={0} autoHeight direction="vertical" >
+                        <SwiperSlide style={{display: "flex", flexDirection: "column"}}>{child}</SwiperSlide>
+                        <SwiperSlide>answer</SwiperSlide>
+                    </Swiper>
                 </SwiperSlide>
             ))}
         </Swiper>
