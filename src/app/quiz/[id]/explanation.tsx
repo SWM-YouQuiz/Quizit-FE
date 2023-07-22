74790fb29e2381d389ea710eecbc25b656a0d710
entrolEC <euichan1108@gmail.com>
2023-07-22 16:55:50 +0900

    Feat: 챗봇 streaming 및 프롬프트 구현

--- src/app/quiz/[id]/explanation.tsx
+++ src/app/quiz/[id]/explanation.tsx
@@ -1,24 +1,60 @@
+"use client"
+
 import React from "react";
 import {explanationDummy, noData} from "@/modules/quiz/explanationDummy";
+import {useChat, useCompletion} from "ai/react";
 
-const getExplanationApi = async (quizId: number) => {
+const getExplanationApi = (quizId: number) => {
     if(quizId < 0 || explanationDummy.length-1 < quizId) {
         return noData;
     } else {
-        await new Promise((resolve) =>
-            setTimeout(() => resolve(null), 2000)
-        )
         return explanationDummy[quizId];
     }
 }
 
-const ExplanationComponent = async ({quizId}: {quizId: number}) => {
-    const quiz = await getExplanationApi(quizId);
+const systemPrompt = `
+당신은 퀴즈에 대한 해설을 해주는 역할입니다.
+퀴즈의 형식은 아래와 같습니다.
+사용자에게 정답의 번호를 언급해서는 안됩니다. 대신에 정답의 내용(item_content)를 언급하세요.
+content: 퀴즈 내용
+items: 퀴즈 문항들
+answer: 정답 (0~3)
+`
 
-    const {explanation} = quiz;
+const userPrompt = `
+아래 퀴즈에 대한 해설을 부탁합니다.
+그리고 이전 대화 내용을 묻는 질문에 대한 대답을 피해주세요.
+    `
+const invisibleMessageId = 'invisible'
+const isInvisibleMessage = (id: string) => id === invisibleMessageId;
+
+const ExplanationComponent = ({quizId}: {quizId: number}) => {
+    const { messages, input, handleInputChange, handleSubmit } = useChat({
+        initialMessages: [
+            {id: invisibleMessageId, "role": "system", "content": systemPrompt},
+            {id: invisibleMessageId, role: "user", content: userPrompt},
+            {id: `quiz-${quizId}-2`, role: "assistant", content: getExplanationApi(quizId).explanation}
+        ]
+    })
+
     return (
         <div>
-            {explanation}
+            <form onSubmit={handleSubmit}>
+                    <input
+                        className="fixed w-full max-w-md bottom-0 border border-gray-300 rounded mb-8 shadow-xl p-2"
+                        value={input}
+                        placeholder="Describe your business..."
+                        onChange={handleInputChange}
+
+                    />
+            </form>
+            {messages.map(m => !isInvisibleMessage(m.id) && (
+                    <div key={m.id}>
+                        {m.role === 'user' ? 'User: ' : 'AI: '}
+                        {m.content}
+                    </div>
+                )
+            )}
         </div>
     )
 }
--- src/modules/quiz/explanationDummy.ts
+++ src/modules/quiz/explanationDummy.ts
@@ -4,22 +4,11 @@ export const noData: Explanation = {
     explanation: "해설이 없습니다."
 }
 
-export const explanationDummy: Explanation[] =
-
-    [{'explanation': '이 코드는 Spring Boot 애플리케이션의 기본 구조로, 클래스와 메서드 선언, 어노테이션 사용이 모두 올바르게 이루어져 있습니다.'},
-    {'explanation': '`spring-boot-starter-webflux` 모듈은 비동기 네트워킹 라이브러리를 제공하고, Reactive Web 애플리케이션 개발에 사용됩니다. 전통적인 서블릿 기반의 웹 애플리케이션 개발에는 사용되지 않습니다.'},
-    {'explanation': 'Spring Boot는 Tomcat, Jetty, Undertow, 그리고 Netty와 같은 임베디드 서버를 지원합니다. 이들 모두 HTTP 서버로 사용 가능하므로, Netty는 임베디드 서버로 사용할 수 있습니다.'},
-    {'explanation': 'Java에서 메인 메서드의 서명은 `public static void main(String[] args)`입니다. 이 메서드는 자바 애플리케이션의 진입점 역할을 합니다.'},
-    {'explanation': '이 코드는 Spring Boot를 사용하여 RESTful 웹 서비스를 생성하는 예시로, 클래스와 메서드 선언, 어노테이션 사용이 모두 올바르게 이루어져 있습니다.'},
-    {'explanation': '`spring-boot-starter-web` 모듈은 전통적인 서블릿 기반의 웹 애플리케이션 개발에 사용됩니다. 비동기 네트워킹 라이브러리 제공이나 Reactive Web 애플리케이션 개발, Spring WebFlux 지원은 이 모듈의 기능이 아닙니다.'},
-    {'explanation': 'Spring Boot 웹 애플리케이션 개발에는 주로 Tomcat, Jetty, Netty 등의 임베디드 서버가 사용됩니다. Apache는 이러한 목적으로 일반적으로 사용되지 않습니다.'},
-    {'explanation': '이 코드는 Spring Boot 애플리케이션의 기본 구조로, 클래스와 메서드 선언, 어노테이션 사용이 모두 올바르게 이루어져 있습니다.'}]
-
-// [{'explanation': 'Server Components는 서버에서 렌더링되며, Client Components는 클라이언트에서 렌더링됩니다. 이 차이는 각 컴포넌트의 사용 케이스와 성능에 영향을 미칩니다.'},
-// {'explanation': 'Next.js는 서버 사이드 렌더링(SSR)을 지원하여 애플리케이션의 초기 로드 시간을 줄일 수 있습니다. 이 외에도 Next.js는 라우팅, 코드 분할 등 다양한 기능을 제공합니다.'},
-// {'explanation': 'Server Components는 서버에서 렌더링되기 때문에 클라이언트의 부하를 줄일 수 있습니다. 또한, 클라이언트 측 JavaScript 번들의 크기를 줄일 수 있어, 애플리케이션의 성능을 향상시킬 수 있습니다.'},
-// {'explanation': '이 JavaScript 코드는 React의 상태 훅인 useState를 사용한 함수형 컴포넌트입니다. 코드는 잘 작성되어 있으며, 오류가 없습니다.'},
-// {'explanation': '이 JavaScript 코드는 React의 상태 훅인 useState와 생명주기 훅인 useEffect를 사용한 함수형 컴포넌트입니다. useEffect 함수는 의존성 배열이 없으므로, 컴포넌트가 리렌더링될 때마다 실행됩니다. 이는 의도한 동작이 아닐 수 있습니다.'},
-// {'explanation': 'React와 Next.js의 주요 차이점 중 하나는 렌더링 방식입니다. React는 클라이언트 사이드 렌더링을 지원하는 반면, Next.js는 서버 사이드 렌더링(SSR)과 정적 사이트 생성(SSG)을 지원합니다. 이로 인해 Next.js는 초기 페이지 로드 시간을 개선하고 SEO를 향상시킬 수 있습니다.'},
-// {'explanation': 'Next.js는 파일 기반 라우팅을 지원합니다. 이는 `pages` 디렉토리에 있는 JavaScript 파일을 사용하여 구현됩니다. 각 파일은 해당 파일의 경로에 따라 URL 경로를 자동으로 생성합니다.'}]
-//
+export const explanationDummy: Explanation[] = [{'explanation': '`fetch()`는 데이터를 가져오는 데 사용되며, 서버 컴포넌트에서 데이터를 전송하는 데 사용되지 않습니다.'},
+    {'explanation': '`fetchData` 함수는 서버에서 응답한 JSON 데이터를 반환합니다. `response.json()`은 응답 본문을 JSON으로 해석하여 반환합니다.'},
+    {'explanation': 'Next.js는 `fetch`를 사용할 때 모든 요청을 자동으로 캐싱하지 않습니다. 각 요청이 자체 캐싱과 재검증을 설정할 수 있도록 `fetch` 옵션 객체를 확장합니다.'},
+    {'explanation': "`await` 키워드는 Promise가 해결될 때까지 실행을 일시 중단하므로, 로그는 'Fetching data...', 'Data fetched', 'Data processed' 순서로 출력됩니다."},
+    {'explanation': 'Next.js는 `fetch()`를 사용할 때 자동으로 요청을 캐싱하고 재검증하지 않습니다. 각 요청이 자체 캐싱과 재검증을 설정할 수 있도록 `fetch` 옵션 객체를 확장합니다.'},
+    {'explanation': '`fetchData` 함수에서 `response` 변수는 `fetch` 요청의 Promise 결과를 받습니다. 이후 `response.json()`을 통해 JSON 데이터를 추출할 수 있습니다.'},
+    {'explanation': '`fetch()` 웹 API는 Next.js 서버 컴포넌트에서 데이터를 가져오는 데 사용할 수 있습니다. Next.js App Router를 사용하면 React 컴포넌트에서 직접 데이터를 가져올 수 있습니다.'},
+    {'explanation': '`getData` 함수는 서버에서 응답한 HTTP 상태 코드를 반환합니다. `response.status`는 HTTP 응답의 상태 코드를 반환합니다.'}]
