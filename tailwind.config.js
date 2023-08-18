fb876bf207aec7dc37efec141a0c51e6c35db6c0
entrolEC <euichan1108@gmail.com>
2023-08-18 17:03:56 +0900

    Feat: 색상 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,13 +15,25 @@ module.exports = {
             'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        'bg-primary': '#F1F5F9',
+        'bg-primary': '#F8F8F8',
         'bg-secondary': '#D9D9D9',
 
-        'primary': '#5D5FEF',
-        'secondary': '#A5A6F6',
+        'point1': '#7A72EE',
+        'point2': '#64D188',
+          'point3': '#49AF86',
         'success': '#75D6FF',
-        'error': '#FF7A7A'
+        'error': '#FF7A7A',
+
+          primary: {
+            50: '#F7F5FD',
+              200: '#DDD9F6',
+              400: '#BCB3ED',
+              600: '#9A8DE5',
+              800: '#7967DC',
+              900: '#5741D3'
+          },
+        'black': '#0B0434',
+
       }
     }
   },
