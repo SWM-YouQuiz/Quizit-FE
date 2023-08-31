6cb53385d6ab54edc39ae7789d4055033695962d
entrolEC <euichan1108@gmail.com>
2023-08-31 09:38:13 +0900

    Fix: 레이아웃 버그 수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,6 +4,7 @@ import {ReactNode} from "react";
 const Layout = ({children}: {children: ReactNode}) => {
     return (
         <div className="flex-grow relative max-h-[calc(100dvh-64px)]">
+            {children}
             <Navbar/>
         </div>
     )
