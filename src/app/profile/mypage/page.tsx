f1a6a2d2fdbf9036ff6bed3468e52daaa9032a68
entrolEC <euichan1108@gmail.com>
2023-09-08 23:23:44 +0900

    Fix: 마이페이지 헤더 레이아웃 수정

--- src/app/profile/mypage/page.tsx
+++ src/app/profile/mypage/page.tsx
@@ -11,7 +11,6 @@ const MyPage = () => {
                     <BackArrow/>
                 </Link>
                 <div className="font-bold">마이페이지</div>
-                <div/>
             </Header>
             <div className="flex-grow overflow-y-auto p-5 bg-white">
                 <BodyContainer/>
