5eba3e8068975cc86ac2abd2bdbb0afbad3c90ff
entrolEC <euichan1108@gmail.com>
2023-10-18 13:15:49 +0900

    Fix: 공유 버그 수정

--- src/modules/quiz/components/ShareButton.tsx
+++ src/modules/quiz/components/ShareButton.tsx
@@ -4,21 +4,25 @@ import React from "react";
 
 const ShareButton = () => {
     const handleClick = () => {
-        alert(JSON.stringify(navigator.share))
-        if (navigator.share) {
-            navigator.share({
-                title: "Quiz IT",
-                text: "Quiz IT에서 퀴즈를 해결해보세요.",
-                url: window.location.href,
-            })
-                .then(() => console.log('Successful share'))
-                .catch((error) => console.log('Error sharing', error));
-        } else if (navigator.clipboard) {
-            navigator.clipboard.writeText(window.location.href)
-                .then(() => {
-                    alert("클립보드에 링크가 복사되었습니다.")
-                })
+        const url = window.location.href;
+
+        const tempInput = document.createElement("input");
+        tempInput.value = url;
+        document.body.appendChild(tempInput);
+
+        tempInput.select();
+        tempInput.setSelectionRange(0, 99999);
+
+        try {
+            const successful = document.execCommand('copy');
+            const msg = successful ? 'successful' : 'unsuccessful';
+            alert("클립보드에 링크가 복사되었습니다.");
+        } catch (err) {
+            console.log('Unable to copy the URL, error: ', err);
         }
+
+        // 임시 요소를 제거합니다.
+        document.body.removeChild(tempInput);
     }
 
     return (
