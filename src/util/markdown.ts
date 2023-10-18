2c7b266972b23155635a17ec23862ea317b37958
entrolEC <euichan1108@gmail.com>
2023-10-18 18:31:59 +0900

    Fix: 마크다운 버그 수정

--- src/util/markdown.ts
+++ src/util/markdown.ts
@@ -4,11 +4,12 @@ import rehypePrism from "rehype-prism-plus";
 import rehypeStringify from "rehype-stringify";
 
 export const markdownToHtmlString = async (markdownText: string) => {
-    const unescapedMarkdown = markdownText.replace(/\\`\\`\\`/g, '```');
+    const correctedMarkdown = markdownText.replace(/(\*\*[^*]+\*\*)(\S)/g, '$1 $2');
+
     const processedContent = await remark()
         .use(remarkRehype)
         .use(rehypePrism)
         .use(rehypeStringify)
-        .process(unescapedMarkdown);
+        .process(correctedMarkdown);
     return processedContent.toString();
-}
+};
