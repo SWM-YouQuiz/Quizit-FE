9bd21cfda287ba8502cb7b6a1876d720e3db18f9
entrolEC <euichan1108@gmail.com>
2023-10-19 19:02:44 +0900

    Fix: 마크다운 코드블록 버그 수정

--- src/util/markdown.ts
+++ src/util/markdown.ts
@@ -4,7 +4,10 @@ import rehypePrism from "rehype-prism-plus";
 import rehypeStringify from "rehype-stringify";
 
 export const markdownToHtmlString = async (markdownText: string) => {
-    const correctedMarkdown = markdownText.replace(/(\*\*[^*]+\*\*)(\S)/g, '$1 $2');
+    const correctedMarkdown = markdownText
+        .replace(/(\*\*[^*]+\*\*)(\S)/g, '$1 $2')
+        .replace(/\\`\\`\\`/g, '```');
+
 
     const processedContent = await remark()
         .use(remarkRehype)
