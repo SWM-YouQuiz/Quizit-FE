c7fbc5158024900701c7bc9e0746837df53c9eeb
entrolEC <euichan1108@gmail.com>
2023-10-22 04:51:41 +0900

    Fix: 아이콘 및 스플래시 스크린 수정

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -15,29 +15,38 @@ const Head = () => {
             <meta name="msapplication-tap-highlight" content="no" />
             <meta name="theme-color" content="#5741D3" />
 
-            <link rel="apple-touch-icon" sizes="20x20" href="/icons/iOS/AppIcon.appiconset/Icon-20x20@1x.png" />
-            <link rel="apple-touch-icon" sizes="29x29" href="/icons/iOS/AppIcon.appiconset/Icon-29x29@1x.png" />
-            <link rel="apple-touch-icon" sizes="40x40" href="/icons/iOS/AppIcon.appiconset/Icon-20x20@2x.png" />
-            <link rel="apple-touch-icon" sizes="50x50" href="/icons/iOS/AppIcon.appiconset/Icon-50x50@1x.png" />
-            <link rel="apple-touch-icon" sizes="58x58" href="/icons/iOS/AppIcon.appiconset/Icon-29x29@2x.png" />
-            <link rel="apple-touch-icon" sizes="60x60" href="/icons/iOS/AppIcon.appiconset/Icon-20x20@3x.png" />
-            <link rel="apple-touch-icon" sizes="76x76" href="/icons/iOS/AppIcon.appiconset/Icon-76x76@1x.png" />
-            <link rel="apple-touch-icon" sizes="80x80" href="/icons/iOS/AppIcon.appiconset/Icon-40x40@2x.png" />
-            <link rel="apple-touch-icon" sizes="87x87" href="/icons/iOS/AppIcon.appiconset/Icon-29x29@3x.png" />
-            <link rel="apple-touch-icon" sizes="100x100" href="/icons/iOS/AppIcon.appiconset/Icon-50x50@2x.png" />
-            <link rel="apple-touch-icon" sizes="120x120" href="/icons/iOS/AppIcon.appiconset/Icon-40x40@3x.png" />
-            <link rel="apple-touch-icon" sizes="120x120" href="/icons/iOS/AppIcon.appiconset/Icon-60x60@2x.png" />
-            <link rel="apple-touch-icon" sizes="152x152" href="/icons/iOS/AppIcon.appiconset/Icon-76x76@2x.png" />
-            <link rel="apple-touch-icon" sizes="180x180" href="/icons/iOS/AppIcon.appiconset/Icon-60x60@3x.png" />
+            <link rel="apple-touch-icon" sizes="20x20" href="/icons/ios/20.png" />
+            <link rel="apple-touch-icon" sizes="29x29" href="/icons/ios/29.png" />
+            <link rel="apple-touch-icon" sizes="32x32" href="/icons/ios/32.png" />
+            <link rel="apple-touch-icon" sizes="40x40" href="/icons/ios/40.png" />
+            <link rel="apple-touch-icon" sizes="50x50" href="/icons/ios/50.png" />
+            <link rel="apple-touch-icon" sizes="57x57" href="/icons/ios/57.png" />
+            <link rel="apple-touch-icon" sizes="58x58" href="/icons/ios/58.png" />
+            <link rel="apple-touch-icon" sizes="60x60" href="/icons/ios/60.png" />
+            <link rel="apple-touch-icon" sizes="64x64" href="/icons/ios/64.png" />
+            <link rel="apple-touch-icon" sizes="72x72" href="/icons/ios/72.png" />
+            <link rel="apple-touch-icon" sizes="76x76" href="/icons/ios/76.png" />
+            <link rel="apple-touch-icon" sizes="80x80" href="/icons/ios/80.png" />
+            <link rel="apple-touch-icon" sizes="87x87" href="/icons/ios/87.png" />
+            <link rel="apple-touch-icon" sizes="100x100" href="/icons/ios/100.png" />
+            <link rel="apple-touch-icon" sizes="114x114" href="/icons/ios/114.png" />
+            <link rel="apple-touch-icon" sizes="120x120" href="/icons/ios/120.png" />
+            <link rel="apple-touch-icon" sizes="128x128" href="/icons/ios/128.png" />
+            <link rel="apple-touch-icon" sizes="144x144" href="/icons/ios/144.png" />
+            <link rel="apple-touch-icon" sizes="152x152" href="/icons/ios/152.png" />
+            <link rel="apple-touch-icon" sizes="167x167" href="/icons/ios/167.png" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/icons/ios/180.png" />
+            <link rel="apple-touch-icon" sizes="192x192" href="/icons/ios/192.png" />
+            <link rel="apple-touch-icon" sizes="256x256" href="/icons/ios/256.png" />
+            <link rel="apple-touch-icon" sizes="512x512" href="/icons/ios/512.png" />
+            <link rel="apple-touch-icon" sizes="1024x1024" href="/icons/ios/1024.png" />
 
-
-            <link rel="icon" type="image/png" sizes="36x36" href="/icons/android/drawable-ldpi/icon.png" />
-            <link rel="icon" type="image/png" sizes="48x48" href="/icons/android/drawable-mdpi/icon.png" />
-            <link rel="icon" type="image/png" sizes="72x72" href="/icons/android/drawable-hdpi/icon.png" />
-            <link rel="icon" type="image/png" sizes="96x96" href="/icons/android/drawable-xhdpi/icon.png" />
-            <link rel="icon" type="image/png" sizes="96x96" href="/icons/android/drawable/icon.png" />
-            <link rel="icon" type="image/png" sizes="144x144" href="/icons/android/drawable-xxhdpi/icon.png" />
-            <link rel="icon" type="image/png" sizes="192x192" href="/icons/android/drawable-xxxhdpi/icon.png" />
+            <link rel="icon" type="image/png" sizes="48x48" href="/icons/android/android-launchericon-48-48.png" />
+            <link rel="icon" type="image/png" sizes="72x72" href="/icons/android/android-launchericon-72-72.png" />
+            <link rel="icon" type="image/png" sizes="96x96" href="/icons/android/android-launchericon-96-96.png" />
+            <link rel="icon" type="image/png" sizes="144x144" href="/icons/android/android-launchericon-144-144.png" />
+            <link rel="icon" type="image/png" sizes="192x192" href="/icons/android/android-launchericon-192-192.png" />
+            <link rel="icon" type="image/png" sizes="512x512" href="/icons/android/android-launchericon-512-512.png" />
 
             <link rel="manifest" href="/manifest.json" />
             {/*<link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />*/}
@@ -57,16 +66,36 @@ const Head = () => {
             <meta property="og:url" content="https://quiz.org" />
             <meta property="og:image" content="https://quizit.org/icons/android/drawable-xxxhdpi/icon.png" />
 
-            <link href="/splashscreens/iphone5_splash.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
-            <link href="/splashscreens/iphone6_splash.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
-            <link href="/splashscreens/iphoneplus_splash.png" media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
-            <link href="/splashscreens/iphonex_splash.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
-            <link href="/splashscreens/iphonexr_splash.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
-            <link href="/splashscreens/iphonexsmax_splash.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
-            <link href="/splashscreens/ipad_splash.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
-            <link href="/splashscreens/ipadpro1_splash.png" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
-            <link href="/splashscreens/ipadpro3_splash.png" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
-            <link href="/splashscreens/ipadpro2_splash.png" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+            <meta name="apple-mobile-web-app-capable" content="yes"/>
+
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-2048-2732.jpg" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-2732-2048.jpg" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-1668-2388.jpg" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-2388-1668.jpg" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-1536-2048.jpg" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-2048-1536.jpg" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-1668-2224.jpg" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-2224-1668.jpg" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-1620-2160.jpg" media="(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-2160-1620.jpg" media="(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-1290-2796.jpg" media="(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-2796-1290.jpg" media="(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-1179-2556.jpg" media="(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-2556-1179.jpg" media="(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-1284-2778.jpg" media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-2778-1284.jpg" media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-1170-2532.jpg" media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-2532-1170.jpg" media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-1125-2436.jpg" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-2436-1125.jpg" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-1242-2688.jpg" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-2688-1242.jpg" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-828-1792.jpg" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-1792-828.jpg" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-750-1334.jpg" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-1334-750.jpg" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-640-1136.jpg" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
+            <link rel="apple-touch-startup-image" href="/splashscreens/apple-splash-1136-640.jpg" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" />
 
             <meta
                 name='viewport'
