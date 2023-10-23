792be759e55bfc847a2dcaa2a3c8a9d3adde8dac
entrolEC <euichan1108@gmail.com>
2023-10-23 16:58:27 +0900

    Fix: bundler-analyzer 제거

--- next.config.js
+++ next.config.js
@@ -40,9 +40,4 @@ const withPWA = require('next-pwa')({
     buildExcludes: ["app-build-manifest.json"],
 })
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: true,
-    openAnalyzer: true,
-});
-
 module.exports = withPWA(nextConfig)
