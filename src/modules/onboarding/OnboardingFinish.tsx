8c53d4f83dcb826947729c5b22aab5335260d1f2
entrolEC <euichan1108@gmail.com>
2023-10-25 23:14:22 +0900

    Feat: 랭킹 애니메이션 및 캐싱 버그 수정

--- src/modules/onboarding/OnboardingFinish.tsx
+++ src/modules/onboarding/OnboardingFinish.tsx
@@ -8,19 +8,21 @@ const OnboardingFinish = ({page}: {page: number}) => {
     return (
         <div className="flex-grow overflow-y-auto p-5 bg-white">
             <div className="h-full flex flex-col items-center space-y-2 justify-between">
-                <div className="mt-[180px] flex flex-col items-center">
+                <div className="mt-[160px] flex flex-col items-center">
                     <Image
-                        className="mt-40"
                         src="/characters/onboarding5.svg"
                         alt="목표를 달성한 퀴즈보"
-                        width={240}
-                        height={240}
+                        sizes="100vw"
+                        fill
+                        style={{
+                            objectFit: 'none',
+                        }}
                     />
                     <div className="text-center text-secondary-800 text-lg mb-10 font-semibold px-16 whitespace-break-spaces">{"모든 준비가 끝났어요.\n시작해볼까요?"}</div>
                 </div>
                 <Link
                     href={`/curriculum`}
-                    className="w-full"
+                    className="w-full z-50"
                 >
                     <Button context="다음"/>
                 </Link>
