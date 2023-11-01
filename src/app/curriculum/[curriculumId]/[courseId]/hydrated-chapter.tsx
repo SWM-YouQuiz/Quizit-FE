81605d231a4e977f91f4571d2a876acfa41eadd9
entrolEC <euichan1108@gmail.com>
2023-11-02 02:10:00 +0900

    Feat: prefetchQuery 제거

--- src/app/curriculum/[curriculumId]/[courseId]/hydrated-chapter.tsx
+++ src/app/curriculum/[curriculumId]/[courseId]/hydrated-chapter.tsx
@@ -1,9 +1,7 @@
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 import { ReactNode } from "react";
-import { getChapterProgress } from "@/modules/curriculum/serverApiActions";
 import getQueryClient from "@/lib/reactQuery/get-query-client";
-import { getAccessToken } from "@/modules/serverActions";
 
 type HydratedChaptersProps = {
     children: ReactNode;
@@ -11,11 +9,7 @@ type HydratedChaptersProps = {
 };
 export const HydratedChapters = async ({ children, chapterId }: HydratedChaptersProps) => {
     const queryClient = getQueryClient();
-    const accessToken = await getAccessToken();
-    await queryClient.prefetchQuery({
-        queryKey: ["chapters", chapterId],
-        queryFn: () => getChapterProgress({ chapterId: chapterId, accessToken }),
-    });
+
     const dehydratedState = dehydrate(queryClient);
 
     return <HydrationBoundary state={dehydratedState}>{children}</HydrationBoundary>;
