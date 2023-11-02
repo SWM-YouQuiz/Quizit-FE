a678551fccb8217072d24c1fe5236c287535965a
entrolEC <euichan1108@gmail.com>
2023-11-02 14:48:10 +0900

    Feat: 애니메이션 수정

--- src/modules/curriculum/components/Tag.tsx
+++ src/modules/curriculum/components/Tag.tsx
@@ -12,7 +12,8 @@ const Tag = ({ name, toggle, handleToggle, text, icon }: ToggleButton) => {
                 "inner-border-2 h-8 rounded-lg text-secondary-900 flex justify-between px-3 items-center w-15 bg-white",
                 toggle ? "inner-border-point1 text-point1 stroke-point1" : "inner-border-secondary-400 stroke-secondary-400 text-secondary-400",
             )}
-            whileTap={{ scale: 0.95 }}
+            whileTap={{ scale: 0.95, rotate: [0, 10, -10] }}
+            transition={{ type: "spring", bounce: 0.7 }}
         >
             <div className="flex">
                 {icon}
