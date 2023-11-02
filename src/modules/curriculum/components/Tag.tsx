67b63d827c3ee424bedd1da039d31c2652be79d2
entrolEC <euichan1108@gmail.com>
2023-11-02 20:34:37 +0900

    Fix: 필터 태그 텍스트 스타일 수정

--- src/modules/curriculum/components/Tag.tsx
+++ src/modules/curriculum/components/Tag.tsx
@@ -9,7 +9,7 @@ const Tag = ({ name, toggle, handleToggle, text, icon }: ToggleButton) => {
             name={name}
             onClick={handleToggle}
             className={cn(
-                "inner-border-2 h-8 rounded-lg text-secondary-900 flex justify-between px-3 items-center w-15 bg-white text-sm",
+                "inner-border-2 h-8 rounded-lg text-secondary-900 flex justify-between px-3 items-center w-15 bg-white text-[13px] whitespace-nowrap",
                 toggle ? "inner-border-point1 text-point1 stroke-point1" : "inner-border-secondary-400 stroke-secondary-400 text-secondary-400",
             )}
             whileTap={{ scale: 0.95 }}
