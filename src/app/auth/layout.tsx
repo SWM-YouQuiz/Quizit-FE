fda8a48ab6ac055c52b7add0cdfc298562f8e7ac
entrolEC <euichan1108@gmail.com>
2023-11-07 20:45:27 +0900

    Fix: Iphone 14 pro 로그인 화면 잘림 수정

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 const AuthLayout = async ({ children }: { children: ReactNode }) => {
-    return <div className="flex-grow relative max-h-screen p-4 bg-primary-900">{children}</div>;
+    return <div className="flex-grow relative h-screen p-4 bg-primary-900">{children}</div>;
 };
 
 export default AuthLayout;
