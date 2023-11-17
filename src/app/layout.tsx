58b36428c7d6e8d04265731d20c8858a4a3f00fa
entrolEC <euichan1108@gmail.com>
2023-11-18 00:24:07 +0900

    Feat: font preload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import QuizFilterContainer from "@/modules/curriculum/components/QuizFilterConta
 import { AnimationWrapper } from "@/lib/animation/AnimationWrapper";
 import { ReactQueryProvider } from "@/lib/reactQuery/provider";
 import { HydratedProvider } from "@/lib/reactQuery/hydrated-provider";
+import localFont from "next/font/local";
 
 export const metadata = {
     title: "퀴즈잇",
@@ -18,9 +19,59 @@ export const metadata = {
     },
 };
 
+const font = localFont({
+    src: [
+        {
+            path: "../util/font/PretendardVariable.woff2",
+            weight: "100",
+            style: "normal",
+        },
+        {
+            path: "../util/font/PretendardVariable.woff2",
+            weight: "200",
+            style: "normal",
+        },
+        {
+            path: "../util/font/PretendardVariable.woff2",
+            weight: "300",
+            style: "normal",
+        },
+        {
+            path: "../util/font/PretendardVariable.woff2",
+            weight: "400",
+            style: "normal",
+        },
+        {
+            path: "../util/font/PretendardVariable.woff2",
+            weight: "500",
+            style: "normal",
+        },
+        {
+            path: "../util/font/PretendardVariable.woff2",
+            weight: "600",
+            style: "normal",
+        },
+        {
+            path: "../util/font/PretendardVariable.woff2",
+            weight: "700",
+            style: "normal",
+        },
+        {
+            path: "../util/font/PretendardVariable.woff2",
+            weight: "800",
+            style: "normal",
+        },
+        {
+            path: "../util/font/PretendardVariable.woff2",
+            weight: "900",
+            style: "normal",
+        },
+    ],
+});
+
 const RootLayout = async ({ children }: { children: ReactNode }) => {
     return (
-        <html lang="ko" className="overscroll-none">
+        <html lang="ko" className={`overscroll-none ${font.className}`}>
             <Head />
             <body className="h-screen w-full flex flex-col bg-secondary-50">
                 <ReactQueryProvider>
